996fa42fb06575e4eda5c3636c8c6252d68c6ea8
Jesse Moody <jess.t.moody@gmail.com>
2022-06-20 22:51:00 -0400

    Switched speech recognition to Vosk. Added power down voice command. Program ends if model folder not found.

--- source/BrightBoy.py
+++ source/BrightBoy.py
@@ -1,12 +1,16 @@
 # BrightBoy 2020
 # Jesse Moody
 
-import speech_recognition as sr
+# Portions of code taken from test_microphone.py in Vosk's Python examples
+
+import sounddevice as sd
+import vosk
 import sys
 import threading
 import queue
 import re
 import time
+import os
 try:
     import pantilthat
 except:
@@ -14,20 +18,16 @@ except:
     pantilthat = pantilthatEmulator.Pantilthat() # instantiate Pantilthat object (class)
 killThreads = False
 
-def recognizeWords(r, mic, wordQueue):
-    global killThreads
-    with mic as source:
-        r.adjust_for_ambient_noise(source)
-    while killThreads == False: # keeps thread alive
-        with mic as source:
-            print("Listening for command")
-            audio = r.listen(source)
-        try:
-            words = r.recognize_google(audio)
-            print(words)
-            wordQueue.put(words)
-        except:
-            print("Say what? (Couldn't recognize speech)")
+q = queue.Queue()  # stores streaming audio data from sounddevice
+wordQueue = queue.Queue() # stores speech to text words from recognizeWords()
+endProgram = False # recognizeWords() thread sets to True if model folder doesn't exist
+
+# Taken from Vosk test_microphone.py example
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
 
 # Allows spoken command to be translated into servo command
 def servoDo(command, modifier=22): # modifier defaults to 22 if none was given, which is about 1/4 of 90  
@@ -64,38 +64,67 @@ def servoDo(command, modifier=22): # modifier defaults to 22 if none was given, 
     elif command == "off":
         pantilthat.set_all(0, 0, 0, 0)
         pantilthat.show()
+    elif command == "power down":
+        if 'pi' in str(os.uname()):
+            pantilthat.set_all(0, 0, 0, 0)
+            pantilthat.show()
+            os.system("sudo shutdown -h now")
+        else:
+            print("Not running on RasPi. Aborting shutdown.")
 
     # print angles
     print(f'newPanAngle = {pantilthat.get_pan()}')
     print(f'newTiltAngle = {pantilthat.get_tilt()}')
 
+# Convert audio stream from microphone into text
+def recognizeWords():
+    try:
+        if not os.path.exists("model"):
+            print ("Please download a model for your language from https://alphacephei.com/vosk/models")
+            print ("and unpack as 'model' in the current folder.")
+            global endProgram # access global var previously defined
+            endProgram = True # signal to main thread to end program
+            return
+        model = vosk.Model("model")
+        myDevice = sd.default.device
+        input_device_info = sd.query_devices(None, 'input') # When device arg (1st arg) is None, 
+                                                            # dictionary of default input device info is returned
+        
+        # soundfile expects an int, sounddevice provides a float:
+        samplerate = int(input_device_info['default_samplerate'])
+            
+        dump_fn = None # Normally used for storing rec, but not needed for BB (see test_microphone.py in vosk Python examples)
+
+        # TODO: Pass sd.RawInputStream() the Adafruit Voice Bonnet mic as the device arg
+        with sd.RawInputStream(samplerate, blocksize = 8000, device=myDevice, dtype='int16',
+                                channels=1, callback=callback):
+            rec = vosk.KaldiRecognizer(model, samplerate)
+            while True:
+                data = q.get()
+                if rec.AcceptWaveform(data):
+                    wordQueue.put(rec.Result())
+    except Exception as e:
+        print(e)
+
 if __name__ == "__main__":
-    wordQueue = queue.Queue()
     wakeWords = ["bright boy ", "right boy ", "Bright boy ", "Brightboy ", "bribe oy ",
                  "right boy ", "brightpoint", "white boy", "breakpoint", "brake boy"] # wake word and homonyms
-    commands = ["center", "Center", "up", "down", "right", "left", "on", "off"]
-    modifiers = {"little":11, "bit":11, "smidge":5, "tad":2, "hair":1}
+    commands = ["center", "Center", "up", "down", "right", "left", "on", "off", "power down"]
+    modifiers = {"little":11, "bit":11, "tad":5, "smidge":2, "hair":1}
 
-    # center servos and setup NeoPixel ring
+    # Center servos and setup NeoPixel ring
     pantilthat.pan(0)
     pantilthat.tilt(0)
     pantilthat.light_mode(pantilthat.WS2812)
     pantilthat.light_type(pantilthat.GRBW)
 
     try:
-        r = sr.Recognizer()
-        mic = sr.Microphone(device_index=0) # adafruit voice bonnet device index = 0
-
-        listenerThread = threading.Thread(target=recognizeWords, args=(r, mic, wordQueue)) # constantly listen for new commands
+        # Start speech recognition thread to constantly listen for new commands
+        listenerThread = threading.Thread(target=recognizeWords)
         listenerThread.daemon = True
         listenerThread.start()
 
-        lastTime = 0
-        while True:
-            #timeInt = int(time.time())
-            #if timeInt % 2 == 0 and timeInt != lastTime:
-            #    print("wordQueue is empty")
-            #    lastTime = timeInt
+        while endProgram == False: # endProgram var is controlled by recognizeWords() thread
             if not wordQueue.empty():
                 words = wordQueue.get()
                 print(f'words from queue={words}')
